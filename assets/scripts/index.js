e24a677cc6a64acff38c2c9caa15ab43804e5ebe
Jonmar Neto <jonmarlimaneto@gmail.com>
2025-08-29 17:40:52 -0300

    feat: add random dice roll generator

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,2 +1,8 @@
-let randomNumber1 = Math.floor(Math.random()*6 + 1)
+let randomNumber1 = Math.floor(Math.random() * 6 + 1);
+let randomNumber2 = Math.floor(Math.random() * 6 + 1);
 
+let firstImg = document.querySelector(".img1");
+let secondImg = document.querySelector(".img2")
+
+firstImg.setAttribute("src", `./assets/images/dice${randomNumber1}.png`);
+secondImg.setAttribute("src", `./assets/images/dice${randomNumber2}.png`);
