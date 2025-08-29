37fc582bb4aaecd58fd0fbd09f072577e63a8ae7
Jonmar Neto <jonmarlimaneto@gmail.com>
2025-08-29 18:26:27 -0300

    feat: add game winner determination function

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,22 @@
 let randomNumber1 = Math.floor(Math.random() * 6 + 1);
 let randomNumber2 = Math.floor(Math.random() * 6 + 1);
 
+
 let firstImg = document.querySelector(".img1");
 let secondImg = document.querySelector(".img2")
+let winnerTitle = document.querySelector("h1")
 
 firstImg.setAttribute("src", `./assets/images/dice${randomNumber1}.png`);
 secondImg.setAttribute("src", `./assets/images/dice${randomNumber2}.png`);
+
+function gameWinner() {
+    if (randomNumber1 > randomNumber2) {
+        winnerTitle.textContent = "🚩 Player 1 wins!"; // Alterado para textContent
+    } else if (randomNumber2 > randomNumber1) {
+        winnerTitle.textContent = "Player 2 wins! 🚩";
+    } else {
+        winnerTitle.textContent = "Draw!";
+    }
+}
+
+gameWinner();
